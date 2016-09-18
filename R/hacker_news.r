347e8488a080827a380a0ca9756bbef91cd27f8b
poplav <mikepop77@gmail.com>
2016-09-18 17:23:43 -0500

    extract out base url and fix comments

--- R/hacker_news.r
+++ R/hacker_news.r
@@ -5,7 +5,7 @@ base_url <- "https://hacker-news.firebaseio.com/v0"
 #' This function returns the top stories from hacker news
 #'
 #' @param limit  The top limit stories to return
-#' @param pretty_print  If true print to the terminal and return NULL else return the json structure
+#' @param pretty_print  If true return list of serialized stories else return the json structure
 #' @export
 #' @examples
 #' get_top_stories()
@@ -26,7 +26,7 @@ get_top_stories <- function(limit = 10, pretty_print = FALSE) {
 #' This function returns the story given it's story id
 #'
 #' @param id  The id of the story to query
-#' @param pretty_print  If true print to the terminal and return NULL else return the json structure
+#' @param pretty_print  If true return list of serialized stories else return the json structure
 #' @export
 #' @examples
 #' get_story(12508356)
@@ -34,7 +34,7 @@ get_top_stories <- function(limit = 10, pretty_print = FALSE) {
 #' @importFrom jsonlite fromJSON
 get_story <- function(id, pretty_print = FALSE) {
     story <- {
-        story_url <- paste("https://hacker-news.firebaseio.com/v0/item/", id, ".json?print=pretty", sep="")
+        story_url <- paste(base_url, "/item/", id, ".json", sep="")
         fromJSON(story_url)
     }
     process_stories(list(story), pretty_print)
@@ -42,10 +42,10 @@ get_story <- function(id, pretty_print = FALSE) {
 
 #' process_stories
 #'
-#' This function processes the stories by either pretty_printing or simply returning them
+#' This function processes the stories by either returning the serialized stories or original json structure
 #'
 #' @param stories  The hacker news stories to process
-#' @param pretty_print  If true print to the terminal and return NULL else return the json structure
+#' @param pretty_print  If true return list of serialized stories else return the json structure
 process_stories <- function(stories, pretty_print) {
     if(pretty_print) {
         lapply(stories, function (story) {
